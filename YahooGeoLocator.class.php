b7acd89d3e42170e93ad2dd5f465067ce6c2cce8
Ondrej Podolinsky <podolinek@gmail.com>
2013-06-10 20:25:56 +0200

    updated info about yahoo service

--- YahooGeoLocator.class.php
+++ YahooGeoLocator.class.php
@@ -3,11 +3,8 @@
 * Geo Locator
 * YahooGeoLocator.class.php
 *
-* About class: Class for parsing data from Yahoo GEO locating service. Yahoo API
-* has daily limit 50 000 queries, so it is much more interesting for geo tagging
-* big packet of data. Service is very precise at least in central Europe but
-* unfortunatelly google is more:-/
-* More info about Yahoo APIs on YDN: http://developer.yahoo.com/everything.html
+* About class: Class for parsing data from payed Yahoo BOSS Geo Service.
+* More info about Yahoo APIs on http://developer.yahoo.com/boss/geo
 *
 * @Version:		1.2
 * @Release:		2010-12-22
@@ -17,11 +14,9 @@
 * Copyright (c) 2010-2013, podolinek
 * This class is under GPL Licencense Agreement.
 *
-* I will be pleased for any feedback.)
-*
 */
 class YahooGeoLocator extends GeoLocator {
-	const serviceAddress = 'http://where.yahooapis.com/geocode?location=%s&flags=PJ';//address for calling service
+	const serviceAddress = 'http://where.yahooapis.com/geocode?location=%s&flags=J';//address for calling service
 
 	/**
 	* Initial set of base variables in class
